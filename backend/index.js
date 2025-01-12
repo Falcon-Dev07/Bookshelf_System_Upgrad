98702580fbb5f57915d1dec4a50aa4df786fa2aa
Jyoti <jyotimbhaisare.1990@gmail.com>
2025-01-12 14:55:03 +0530

    Updated commit for CORS error

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,9 @@ app.use(
   })
 );
 
+// Handle preflight OPTIONS requests
+app.options("*", cors());
+
 /*app.use(
   cors({
     origin: "http://localhost:3000", // Replace with your frontend URL
