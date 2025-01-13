9226bdd4856f57895a3a6a79da435df3e6c1a5b8
Jyoti <jyotimbhaisare.1990@gmail.com>
2025-01-13 21:06:36 +0530

    change passport.js for google login

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,8 +5,12 @@ const User = require("../models/User");
 // Determine the callback URL dynamically
 const CALLBACK_URL =
   process.env.REACT_APP_ENV === "production"
-    ? `${process.env.BACKEND_BASE_URL}/auth/google/callback` // Deployed backend
+    ? `${process.env.REACT_APP_BACKEND_URL}/auth/google/callback` // Deployed backend
     : "http://localhost:5000/auth/google/callback"; // Local backend
+/*const CALLBACK_URL =
+  process.env.REACT_APP_ENV === "production"
+    ? `${process.env.BACKEND_BASE_URL}/auth/google/callback` // Deployed backend
+    : "http://localhost:5000/auth/google/callback"; // Local backend*/
 
 /*const CALLBACK_URL =
   process.env.REACT_APP_ENV === "production"
