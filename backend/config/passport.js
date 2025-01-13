845ce014f5a7d9e3d2e1f6cee61afd0aa59352d3
Jyoti <jyotimbhaisare.1990@gmail.com>
2025-01-13 21:32:00 +0530

    change passport.js and .env file for google login

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,8 +5,9 @@ const User = require("../models/User");
 // Determine the callback URL dynamically
 const CALLBACK_URL =
   process.env.REACT_APP_ENV === "production"
-    ? `${process.env.REACT_APP_BACKEND_URL}/auth/google/callback` // Deployed backend
+    ? `${process.env.BACKEND_BASE_URL}/auth/google/callback` // Deployed backend
     : "http://localhost:5000/auth/google/callback"; // Local backend
+
 /*const CALLBACK_URL =
   process.env.REACT_APP_ENV === "production"
     ? `${process.env.BACKEND_BASE_URL}/auth/google/callback` // Deployed backend
@@ -16,6 +17,7 @@ const CALLBACK_URL =
   process.env.REACT_APP_ENV === "production"
     ? `${process.env.REACT_APP_BACKEND_URL}/auth/google/callback` // Use production backend URL from .env
     : `${process.env.REACT_APP_BASE_URL}/auth/google/callback`; // Use local backend URL for development*/
+
 console.log(`Google OAuth Callback URL: ${CALLBACK_URL}`);
 
 passport.use(
